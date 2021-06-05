a300355f62a4b80c2054ca1499079b7b4e5c3f76
Dmitry Petrov <petrovdmitrys@yandex.ru>
2021-06-05 19:01:47 +0500

    First deployment

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "1846606930:AAEZHpZ_dhfM_CUFijArRsWlpDAeh5eu2IE"
 bot_user_name = "studyLuckyBot"
-URL = "the heroku app link that we will create later"
+URL = "https://study-lucky-bot.herokuapp.com/"
